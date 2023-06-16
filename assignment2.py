e43e72ccde09ee2ef1a261e44ab6fb6eae3df450
parshwashah17 <136671167+parshwashah17@users.noreply.github.com>
2023-06-16 10:45:04 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,25 @@
+n1=int(input("Enter the first number:",))
+n2=int(input("Enter the second number:"))
+print("Arithmetic operator:")
+print("exponent:",n1**n2)
+print("Floor division:",n1//n2)
+print("Assignment operator:")
+n1-=1
+print("-=",n1)
+print("Comparison operator:")
+print("<",n1<n2)
+print("Logical operator:")
+a=True
+b=False
+print("or",a or b)
+print("Bitwise operator:")
+print("bitwise XOR",n1^n2)
+print("Identity operator:")
+print("is ", n1 is n2)
+print("Membership operator:")
+a='parshwa'
+print('p' in a)
+print('f' is not a)
+
+
+
